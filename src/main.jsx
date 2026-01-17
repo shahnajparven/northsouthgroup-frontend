2c37d84eef110d53d15f2be38d7bfd5f64f31bba
shahnajparven <shahnajritu21@gmail.com>
2026-01-17 12:39:43 +0600

    change

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <ToastContainer
       position="top-center"
       pauseOnFocusLoss={false}
